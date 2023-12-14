55df634b0655cbd4619f492414fdf43c512a8cf2
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-14 11:17:37 +0530

    message: testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { configClient } from "./config";
 
@@ -19,5 +18,3 @@ root.render(
     <App />
   </Auth0Provider>
 );
-
-reportWebVitals();
