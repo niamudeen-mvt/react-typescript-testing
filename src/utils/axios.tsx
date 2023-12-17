85a7f27cf4112aeb0c2702480e49a10da6875e16
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-17 11:25:04 +0530

    message: text editor set

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://jsonplaceholder.typicode.com";
+const BASE_URL = "http://localhost:5000/api/v1";
+// const BASE_URL = "https://jsonplaceholder.typicode.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
